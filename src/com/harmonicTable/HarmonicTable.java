3600bbd367cd4150440ec8fe94261a2c691b11d6
Bruno Coelho <bcs.cin@gmail.com>
2011-06-05 22:09:07 -0300

    first commit

---
+++ src/com/harmonicTable/HarmonicTable.java
@@ -0,0 +1,31 @@
+package com.harmonicTable;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class HarmonicTable extends Activity {
+    /** Called when the activity is first created. */
+    
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        // Use method "findViewById" to interact (retrieve) with Views from resources.
+        // Button button = (Button) findViewById(R.id.button_id);
+      
+        setContentView(R.layout.main);
+    
+    }
+	
+	/**
+	 * 
+	 * @param The view that was clicked.
+	 */
+	public void buttonClick(View view){
+		int height = view.getHeight();
+		TextView tx = (TextView) findViewById(R.id.textview);
+		tx.setText("The height of the button is: " + height);
+	}
+}
