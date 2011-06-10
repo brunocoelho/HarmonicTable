374e23fbcd313a8bfefb70f94d7a44bd872f7ddd
Bruno Coelho <bcs.cin@gmail.com>
2011-06-10 19:52:04 -0300

    Hexagonos preenchidos

--- src/com/harmonicTable/HarmonicTable.java
+++ src/com/harmonicTable/HarmonicTable.java
@@ -3,24 +3,24 @@ package com.harmonicTable;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
 
 public class HarmonicTable extends Activity {
     /** Called when the activity is first created. */
     
 	@Override
     public void onCreate(Bundle savedInstanceState) {
+		System.out.println("ENTROU NO CONSTRUTOR");
         super.onCreate(savedInstanceState);
         
         // Use method "findViewById" to interact (retrieve) with Views from resources.
         // Button button = (Button) findViewById(R.id.button_id);
       
         setContentView(R.layout.main);
-    
     }
 	
 	/**
 	 * 
+	 * @author Coelho
 	 * @param The view that was clicked.
 	 * @references: http://developer.android.com/guide/topics/media/index.html
 	 */
@@ -31,4 +31,16 @@ public class HarmonicTable extends Activity {
 		
 		<!--android:onClick="buttonClick"-->
 	}*/
+	
+	/**
+	 * 
+	 * @author Coelho
+	 */
+	public void imageClick(View view){
+		System.out.println("Entrou aqui: ID da view = " + view.getId());
+		view.setBackgroundColor(000000);
+		view.setVisibility(0);
+		//Mudar a cor do hexágono qnd ele for clicado
+		//Método que mudar a cor do ImageView -> XML (android:tint) 		Related Method (setColorFilter(int,PorterDuff.Mode))
+	}
 }
