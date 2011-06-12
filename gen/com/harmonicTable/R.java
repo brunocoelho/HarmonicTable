7b478e0c6216ca9c466f028481b903cad6208f18
Bruno Coelho <bcs.cin@gmail.com>
2011-06-12 18:09:46 -0300

    Hexagono diferente

--- gen/com/harmonicTable/R.java
+++ gen/com/harmonicTable/R.java
@@ -11,8 +11,8 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int hexagon=0x7f020000;
-        public static final int hexagono=0x7f020001;
+        public static final int hexa=0x7f020000;
+        public static final int hexagon=0x7f020001;
         public static final int icon=0x7f020002;
     }
     public static final class layout {
