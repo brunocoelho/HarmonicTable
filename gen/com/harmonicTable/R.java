9218714ab08d47a026cab11a2ccc8b93d70640eb
Bruno Coelho <bcs.cin@gmail.com>
2011-06-14 15:14:42 -0300

    Hexagonos mapeados corretamente

--- gen/com/harmonicTable/R.java
+++ gen/com/harmonicTable/R.java
@@ -15,6 +15,44 @@ public final class R {
         public static final int hexagon=0x7f020001;
         public static final int icon=0x7f020002;
     }
+    public static final class id {
+        public static final int L1H1=0x7f050000;
+        public static final int L1H2=0x7f050001;
+        public static final int L1H3=0x7f050002;
+        public static final int L1H4=0x7f050003;
+        public static final int L1H5=0x7f050004;
+        public static final int L2H1=0x7f050014;
+        public static final int L2H2=0x7f050015;
+        public static final int L2H3=0x7f050016;
+        public static final int L2H4=0x7f050017;
+        public static final int L3H1=0x7f050005;
+        public static final int L3H2=0x7f050006;
+        public static final int L3H3=0x7f050007;
+        public static final int L3H4=0x7f050008;
+        public static final int L3H5=0x7f050009;
+        public static final int L4H1=0x7f050018;
+        public static final int L4H2=0x7f050019;
+        public static final int L4H3=0x7f05001a;
+        public static final int L4H4=0x7f05001b;
+        public static final int L5H1=0x7f05000a;
+        public static final int L5H2=0x7f05000b;
+        public static final int L5H3=0x7f05000c;
+        public static final int L5H4=0x7f05000d;
+        public static final int L5H5=0x7f05000e;
+        public static final int L6H1=0x7f05001c;
+        public static final int L6H2=0x7f05001d;
+        public static final int L6H3=0x7f05001e;
+        public static final int L6H4=0x7f05001f;
+        public static final int L7H1=0x7f05000f;
+        public static final int L7H2=0x7f050010;
+        public static final int L7H3=0x7f050011;
+        public static final int L7H4=0x7f050012;
+        public static final int L7H5=0x7f050013;
+        public static final int L8H1=0x7f050020;
+        public static final int L8H2=0x7f050021;
+        public static final int L8H3=0x7f050022;
+        public static final int L8H4=0x7f050023;
+    }
     public static final class layout {
         public static final int main=0x7f030000;
     }
--- src/com/harmonicTable/HarmonicTable.java
+++ src/com/harmonicTable/HarmonicTable.java
@@ -2,45 +2,28 @@ package com.harmonicTable;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
 
 public class HarmonicTable extends Activity {
-    /** Called when the activity is first created. */
     
 	@Override
     public void onCreate(Bundle savedInstanceState) {
-		System.out.println("ENTROU NO CONSTRUTOR");
         super.onCreate(savedInstanceState);
         
-        // Use method "findViewById" to interact (retrieve) with Views from resources.
-        // Button button = (Button) findViewById(R.id.button_id);
-      
         setContentView(R.layout.main);
+        
+        ImageView image = (ImageView) findViewById(R.id.L1H1);
+        image.setOnClickListener(imageListener);
     }
 	
-	/**
-	 * 
-	 * @author Coelho
-	 * @param The view that was clicked.
-	 * @references: http://developer.android.com/guide/topics/media/index.html
-	 */
-	/*public void buttonClick(View view){
-		int height = view.getHeight();
-		TextView tx = (TextView) findViewById(R.id.textview);
-		tx.setText("The height of the button is: " + height);
-		
-		<!--android:onClick="buttonClick"-->
-	}*/
-	
-	/**
-	 * 
-	 * @author Coelho
-	 */
-	public void imageClick(View view){
-		System.out.println("Entrou aqui: ID da view = " + view.getId());
-		view.setBackgroundColor(000000);
-		view.setVisibility(0);
-		//Mudar a cor do hexágono qnd ele for clicado
-		//Método que mudar a cor do ImageView -> XML (android:tint) 		Related Method (setColorFilter(int,PorterDuff.Mode))
-	}
+	private OnClickListener imageListener = new OnClickListener() {
+	    public void onClick(View image) {
+			String saida = "Entrou aqui: ID da view = " + image.getId();
+			Log.e("BRUNO COELHO", saida);
+			image.setVisibility(4);
+	    }
+	};
 }
